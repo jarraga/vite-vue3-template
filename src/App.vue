c0714acfe509ec98c1693e19efe142a71ec7d13c
Jorge Arraga <jorgearraga@gmail.com>
2021-03-07 10:33:25 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="bg-gray-100 text-gray-700 h-screen w-full flex justify-center items-center"
+  >
+    <router-view />
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+@font-face {
+  font-family: nunito;
+  src: url(./assets/Nunito-Regular.ttf);
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>vite.js + vue3 + vuex + router + tailwind.css</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
